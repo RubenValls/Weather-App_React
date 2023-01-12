464d654437097e57ec2c702399cd40cb73945c35
Rubén Valls <r.vallsaparici@gmail.com>
2023-01-12 01:52:32 +0100

    Https to GoogleMaps

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Map = (props) => {
 
-    const mapLink = `http://maps.google.com/maps?q=${props.lat},${props.long}&output=embed`;
+    const mapLink = `https://maps.google.com/maps?q=${props.lat},${props.long}&output=embed`;
 
     return (
         <div class="container mt-5">
