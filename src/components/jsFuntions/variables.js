ef4f6390c67f70773b98898f04db81473c490eed
Rubén Valls <r.vallsaparici@gmail.com>
2023-01-12 02:00:17 +0100

    trying to fix fetch problems

--- src/components/jsFuntions/variables.js
+++ src/components/jsFuntions/variables.js
@@ -6,15 +6,6 @@ export function saveInfo(city, lattitude, longitude){
     cityName = city;
     lat = lattitude;
     lon = longitude;
-
-    const apiKey = "1d29ede4502de56f2857ec5afb7073d8";
-        fetch("http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit="+1+"&appid="+apiKey)
-            .then(response => response.json())
-            .then(response => {
-                lat= response[0].lat;
-                lon= response[0].lon;
-            })
-            .catch(err => console.error(err));
 }
 
 export function getInfo(){
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -11,7 +11,17 @@ const Search = () => {
     const onChange = () => {
         setSearch(document.querySelector("#cityName").value);
         };
-        saveInfo(search, lat, lon);
+    
+    const apiKey = "1d29ede4502de56f2857ec5afb7073d8";
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+search+"&limit="+1+"&appid="+apiKey)
+        .then(response => response.json())
+        .then(response => {
+            setLat(response[0].lat);
+            setLon(response[0].lon);
+        })
+        .catch(err => console.error(err));
+
+    saveInfo(search, lat, lon);
 
     return (
         <div class="container text-center mt-3">
